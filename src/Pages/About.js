3d92aa4a51123e7432d6ed31e0c9c0cd4f3f96e7
kausik <kkausik11@gmail.com>
2024-03-27 20:58:58 +0530

    react router practice with multiple page

---
+++ src/Pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function About() {
+  return (
+      <div class="alert alert-primary" role="alert">
+    <h1>About page</h1>
+</div>
+  )
+}
+
+export default About
---
+++ src/Pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Home() {
+  return (
+      <div class="alert alert-primary" role="alert">
+    <h1>Home page</h1>
+</div>
+  )
+}
+
+export default Home
